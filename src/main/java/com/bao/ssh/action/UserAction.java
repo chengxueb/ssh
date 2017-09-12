8c4b549ef0622fea07ca3a4e468cc734adca78fc
root <root@localhost.localdomain>
2017-09-11 21:01:37 -0700

    ssh

---
+++ src/main/java/com/bao/ssh/action/UserAction.java
@@ -0,0 +1,13 @@
+package com.bao.ssh.action;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserAction {
+	
+	public void detail()
+	{
+		System.out.println("detail");
+	}
+	
+}
